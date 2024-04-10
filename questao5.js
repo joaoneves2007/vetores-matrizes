ffdc82de1189ed71aebb1ea38515bea0c9f4d958
João Neves <contatojotaico@gmail.com>
2024-04-10 10:00:41 -0400

    finalizada questao5

---
+++ questao5.js
@@ -0,0 +1,17 @@
+/*
+Crie uma matriz 3x3 e imprima o elemento na terceira linha e segunda
+coluna em JavaScript?
+*/
+
+// Criando uma matriz 3x3
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+// Acessando o elemento na terceira linha e segunda coluna (índices começam em 0)
+let elemento = matriz[2][1];
+
+// Imprimindo o elemento
+console.log("Elemento na terceira linha e segunda coluna:", elemento);
