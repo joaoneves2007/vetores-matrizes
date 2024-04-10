8d44d0269b11411b93bb174a5b9123e182f6ced5
João Neves <contatojotaico@gmail.com>
2024-04-10 10:00:15 -0400

    finalizada questao1

---
+++ questao1.js
@@ -0,0 +1,10 @@
+/*
+Crie um vetor em JavaScript contendo os números de 1 a 5 e imprima-o
+na tela.
+*/
+
+// Criando o vetor contendo os números de 1 a 5
+const vetor = [1, 2, 3, 4, 5];
+
+// Imprimindo o vetor na tela
+console.log(vetor);
